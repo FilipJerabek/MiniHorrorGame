8b7a54d6a530f457f5c19dd63a27f35d37473a00
FilipJerabek <41015680+FilipJerabek@users.noreply.github.com>
2026-03-27 08:01:56 +0100

    Venkovni prostredi, fukcnost dveri

--- Assets/Free Wood Door Pack/Script/Door.cs
+++ Assets/Free Wood Door Pack/Script/Door.cs
@@ -8,38 +8,47 @@ public class Door : MonoBehaviour
     public bool open;
     public float smooth = 1.0f;
     float DoorOpenAngle = -90.0f;
-    float DoorCloseAngle = 0.0f;
+    private Quaternion closedRotation;
+    private Quaternion openedRotation;
+
 
     public AudioSource asource;
     public AudioClip openDoor, closeDoor;
 
     void Start()
     {
+        closedRotation = transform.localRotation;
+
+        openedRotation = closedRotation * Quaternion.Euler(0, DoorOpenAngle, 0);
+
         asource = GetComponent<AudioSource>();
+
     }
 
     void Update()
     {
-        
+
         if (open)
         {
-            var target = Quaternion.Euler(0, DoorOpenAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, openedRotation, Time.deltaTime * 5 * smooth);
         }
         else
         {
-            var target1 = Quaternion.Euler(0, DoorCloseAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, closedRotation, Time.deltaTime * 5 * smooth);
         }
     }
 
-   
+
     public void OpenDoor()
     {
+        Debug.Log("Někdo na mě sáhnul!");
         open = !open;
-        asource.clip = open ? openDoor : closeDoor;
-        asource.Play();
-    }
+        if (asource != null)
+        {
+            asource.clip = open ? openDoor : closeDoor;
+            asource.Play();
+        }
 
-  
+
+    }
 }
